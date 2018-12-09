ebc3669646def330d2d974a546b70eed7040cee6
Rafael Tamayo Asensio <tamayo@Rafaels-MBP.lan>
2018-12-09 20:34:19 +0000

    reuest param

--- src/main/java/controller/StandardController.java
+++ src/main/java/controller/StandardController.java
@@ -3,15 +3,18 @@ package controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/endpoints")
 public class StandardController {
     Logger logger = LoggerFactory.getLogger(StandardController.class);
 
-    @GetMapping("/")
-    public String greetings(){
+    @GetMapping("/greetings")
+    public String greetings(@RequestParam("name") String name){
         logger.info("Request received");
-        return "Hello ";
+        return "Hello " + name;
     }
 }
